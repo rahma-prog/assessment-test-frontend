002e07be2e68f05f6ba4a4d82ee5664bb2d754d9
rahma <rahmabouzguenda1@gmail.com>
2023-04-19 13:49:18 +0100

    (user)

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import login from "../assets/images/login.jpg";
+import { loginAction } from "../store/storeActions";
+import { useStore } from "../store/storeContext";
 
 export default function LoginPage() {
+  const [email,setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const {dispatch} = useStore("");
+  
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    dispatch(loginAction({email, password}))
+  } 
+  
   return (
     <div className="w-full h-screen flex ">
       <div className="grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[1000px] bg-slate-50">
@@ -21,21 +32,25 @@ export default function LoginPage() {
               </a>
             </h2>
             <div>
-              <label className="font-medium mb-2 flex ">Email</label>
+              <label className="font-medium mb-2 flex " >Email </label>
               <input
                 className="border rounded-md  p-1 w-full shadow  shadow-gray-300 "
                 type="text"
                 placeholder="jane@gmail.com"
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
               />
               <label className="font-medium mb-2 mt-2 flex p">Password</label>
               <input
                 className="rounded-md border p-1  w-full shadow  shadow-gray-300"
                 type="password"
                 placeholder="**********"
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <button className="rounded-md py-1 my-4 text-white bg-green-600 hover:bg-green-500 h-10 w-24">
-              Sign In{" "}
+            <button className="rounded-md py-1 my-4 text-white bg-green-600 hover:bg-green-500 h-10 w-24" onClick={handleSignIn}>
+              Sign In
             </button>
           </form>
         </div>
